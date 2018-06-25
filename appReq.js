9048cff6f3a31bc1e815fd3ff79985e3a1ac6063
osirisbest <osirisbest@gmail.com>
2018-06-25 17:50:09 +0300

    app

---
+++ appReq.js
@@ -0,0 +1,34 @@
+//Test connection to 1C
+const http = require('http');
+let username = "sd", passw = "123", path = '/omnic/hs/ping?t=1'
+
+let host = 'localhost'
+var options = {
+    host,
+    port: 80,
+    path,
+    // authentication headers
+    headers: {
+        'Authorization': 'Basic c2Q6MTEx'// + new Buffer(username + ':' + passw).toString('base64')
+    }
+};
+
+//this is the call
+function doGet() {
+    let request = http.get(options, function (res) {
+        var body = "";
+        res.on('data', function (data) {
+            body += data;
+        });
+        res.on('end', function () {
+            //here we have the full response, html or json object
+            console.log(body);
+        })
+        res.on('error', function (e) {
+            onsole.log("Got error: " + e.message);
+        });
+    }
+    )
+}
+
+setInterval(() => { doGet() }, 3000)
